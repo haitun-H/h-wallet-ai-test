c651db88295c9607721d6722919a97bee608b7ce
haitun-H <haitun858588@gmail.com>
2026-04-20 03:40:24 +0700

    [前端开发Bot] 自动提交: src/screens/RegisterScreen.tsx

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Provider as PaperProvider } from 'react-native-paper';
-import Toast from 'react-native-toast-message';
-import RegisterScreen from './src/screens/RegisterScreen';
-import { toastConfig } from './src/components/ToastConfig';
-
-export default function App() {
-  return (
-    <PaperProvider>
-      <StatusBar style="auto" />
-      <RegisterScreen />
-      <Toast config={toastConfig} />
-    </PaperProvider>
-  );
+{
+  "name": "h-wallet",
+  "version": "1.0.0",
+  "main": "node_modules/expo/AppEntry.js",
+  "scripts": {
+    "start": "expo start",
+    "android": "expo start --android",
+    "ios": "expo start --ios",
+    "web": "expo start --web"
+  },
+  "dependencies": {
+    "expo": "~50.0.0",
+    "expo-clipboard": "~5.0.0",
+    "react": "18.2.0",
+    "react-native": "0.73.0",
+    "react-native-paper": "^5.12.5",
+    "react-native-toast-message": "^2.2.0",
+    "react-native-safe-area-context": "4.8.2",
+    "react-native-screens": "~3.29.0"
+  },
+  "devDependencies": {
+    "@babel/core": "^7.20.0",
+    "@types/react": "~18.2.45",
+    "typescript": "^5.1.3"
+  },
+  "private": true
 }
